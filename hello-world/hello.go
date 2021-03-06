6315b8d846d95e9ffa33e4f8d3f6bd247732d45b
Ki Song <kiwisquash@gmail.com>
2021-03-06 13:52:48 -0800

    refactor: separate prefix generation from greetings function

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -8,12 +8,21 @@ const frenchGreetingsPrefix = "Bonjour, "
 const koreanGreetingsPrefix = "안녕, "
 const greetingsSuffix = "!"
 
+// Hello is a public function
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "world"
 	}
 
-	prefix := englishGreetingsPrefix
+	return getPrefix(language) + name + greetingsSuffix
+}
+
+// getPrefix is a private function
+func getPrefix(language string) (prefix string) {
+	// the named output variable does a couple of things
+	// 1. defines a variable `prefix`, sets it equal to the "zero value" of the type
+	// 2. returns it if there is a `return ` statement without anything specified
+	// 3. will be displayed in the go docs
 
 	switch language {
 		case "Korean": 
@@ -22,9 +31,11 @@ func Hello(name string, language string) string {
 			prefix = spanishGreetingsPrefix
 		case "French": 
 			prefix = frenchGreetingsPrefix
-	}
+		default:
+			prefix = englishGreetingsPrefix
+	}	
 
-	return prefix + name + greetingsSuffix
+	return
 }
 
 func main() {
