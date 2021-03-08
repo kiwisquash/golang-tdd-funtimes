a61a908e129e4b598c2986950c35e5ad6dec2a98
Ki Song <kiwisquash@gmail.com>
2021-03-07 16:39:59 -0800

    feat: added sumTail and sumAll functions and their tests

--- array-and-slice/sum_array.go
+++ array-and-slice/sum_array.go
@@ -7,3 +7,23 @@ func SumArray(arr []int) int {
 	}
 	return sum
 }
+
+func SumAll(collection ...[]int) []int {
+	var output []int
+	for _, c := range collection {
+		output = append(output, SumArray(c))
+	}
+	return output
+}
+
+func SumTail(collection ...[]int) []int {
+	var tailSums []int
+	for _, c := range collection {
+		if len(c) > 0 {
+			tailSums = append(tailSums, SumArray(c[1:]))
+		} else {
+			tailSums = append(tailSums, 0)
+		}
+	}
+	return tailSums
+}
--- array-and-slice/sum_array_test.go
+++ array-and-slice/sum_array_test.go
@@ -1,8 +1,11 @@
 package array_and_slice
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestArraySum (t *testing.T) {
+func TestSum (t *testing.T) {
 
 	t.Run("add 5 numbers", func (t *testing.T) {
 		array := []int{1,2,3,4,5}
@@ -11,5 +14,71 @@ func TestArraySum (t *testing.T) {
 		if got != want {
 			t.Errorf("numbers %v expected %d but got %d", array, want, got)
 		}
+	})
+}
+
+func TestSumAll (t *testing.T) {
+
+	compareSlices := func(got []int , want []int, t testing.TB) {
+		t.Helper()
+
+		if len(got) != len(want) {
+			t.Errorf("failed")
+		}
+
+		for i, v := range(got) {
+			if want[i] != v {
+				t.Errorf("failed")
+			}
+		}
+	}
+
+	t.Run("multiple slices", func (t *testing.T) {
+		slice1 := []int{3,5,7}
+		slice2 := []int{2,6,3,-1}
+
+		got := SumAll(slice1, slice2)
+		want := []int{15, 10}
+
+		compareSlices(got, want, t)
+	})
+
+	t.Run("one slice", func (t *testing.T) {
+		slice := []int{3,5,7}
+
+		got := SumAll(slice)
+		want := []int{15}
+
+		compareSlices(got, want, t)
+
+	})
+}
+
+func TestSumTail (t *testing.T) {
+
+	assertEqual := func(got, want []int, t testing.TB) {
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("want %v, got %v", want, got)
+		}	
+	}
+
+	t.Run("non-emtpy slices", func(t *testing.T) {
+		slice1 := []int{3,5,7}
+		slice2 := []int{2,6,3,-1}
+
+		got := SumTail(slice1, slice2)
+		want := []int{12, 8}
+
+		assertEqual(got, want, t)
+	})
+
+	t.Run("use null slice", func(t *testing.T) {
+		slice1 := []int{}
+		slice2 := []int{2,6,3,-1}
+
+		got := SumTail(slice1, slice2)
+		want := []int{0, 8}
+
+		assertEqual(got, want, t)
 	})
 }
